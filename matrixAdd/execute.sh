23d45b5147965e1014e441532762fa0fd4861140
mathiashuygen <mathias.huygen@outlook.com>
2025-02-18 19:26:26 +0100

    changed the docker file and shell script to have a cleaner way of assigning report names and deleting the reports when the container executes

--- matrixAdd/execute.sh
+++ matrixAdd/execute.sh
@@ -1,11 +1,10 @@
 cd ~
-cd Documents
-DIR="~/cudaReports"
+DIR="cudaReports"
+FILE="matrix_add_report.ncu-rep"
 
-if[ -d "$DIR"]; then
-  sudo rm -rf cudaReports
+if [ -f "$DIR/$FILE" ]; then
+    sudo rm cudaReports/"$FILE"
 fi
-
-cd /ownCudaPrograms/matrixAdd
+cd ownCudaPrograms/matrixAdd
 docker build -t matrix_adder .
-docker run --rm --gpus all --user root -v ~/cudaReports:/home/dockeruser/app/reports matrix_adder
+docker run --rm --gpus all --user root -e REPORT_NAME="$FILE" -v ~/cudaReports:/home/dockeruser/app/reports matrix_adder
