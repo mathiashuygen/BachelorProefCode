c1eaaab57d147ab4f2d98294ec65461c5d63784e
mathiashuygen <mathias.huygen@outlook.com>
2025-03-26 22:22:08 +0100

    more code clean-up

--- scheduler/main.cu
+++ scheduler/main.cu
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <cuda_runtime.h>
 #include <memory>
 #include "Tasks/task.h"
 #include "Jobs/printJob.h"
@@ -11,30 +9,27 @@
 int main(){
   
   
-  std::vector<Task*> tasks;
+  std::vector<Task> tasks;
 
-  auto printJobFactory = JobFactory<PrintJob, int, int>::create(10, 10); 
+  auto printJobFactory = JobFactory<PrintJob, int, int>::create(10, 10);
   auto busyJobFactory = JobFactory<BusyJob, int, int>::create(10, 10);
 
-  Task task1(10, 10, 20, 70, std::move(printJobFactory), 1);
-  Task task2(10, 5, 20, 100, std::move(busyJobFactory), 2);
 
-  tasks.push_back(&task1);
-  tasks.push_back(&task2);
+  tasks.push_back(Task(10, 10, 20, 70, std::move(printJobFactory), 1));
+  tasks.push_back(Task(10, 10, 60, 100, std::move(busyJobFactory), 1));
 
   JLFP scheduler1;
-  
+
   while(true){
 
-    for(Task* task : tasks){
-      if(task->isJobReady()){
-        scheduler1.addJob(task->releaseJob());
+    for(Task& task : tasks){
+      if(task.isJobReady()){
+        scheduler1.addJob(task.releaseJob());
       }
     }
     scheduler1.displayQueueJobs();
     scheduler1.dispatch();
     sleep(2000);
-
   }
 
 
